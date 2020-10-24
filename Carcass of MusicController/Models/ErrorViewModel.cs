ebfeb7a2a4593284311762b4a2a0012eb286e5cf
Николай <barness1985@gmail.com>
2020-10-24 14:10:16 +0300

    Добавьте файлы проекта.

---
+++ Carcass of MusicController/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Carcass_of_MusicController.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
